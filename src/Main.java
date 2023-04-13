97ed8ae04156bc04053414fa8afbf36cad09f38f
action0709 <action0709@mail.ru>
2023-04-13 14:39:04 +0400

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+      Person  person = new Person("Lyapis", "Trubetskoy");
+        Person.changePerson(person);
+        System.out.println(person);
+
     }
+//    Integer[] value={3,4};
+//        for (int i = 0; i < value.length; i++) {
+//            System.out.println("value = " + value[i]);
+//        }
+//    ServiceClass.changeValue(value);
+//        for (int i = 0; i < value.length; i++) {
+//            System.out.println("value = " + value[i]);
+//        }
+//    }
 }
---
+++ src/ServiceClass.java
@@ -0,0 +1,15 @@
+public class ServiceClass {
+
+    public static void changeValue(Integer value[]) {
+        value[0]=99;
+
+//        for (int i = 0; i < value.length; i++) {
+//            if (value[i] != null) {
+//                value[i] = i + 1;
+//            }
+//        }
+
+    }
+}
+
+
